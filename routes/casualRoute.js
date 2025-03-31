7c709b379a7f2a9a412e45b0a3cc2845e12ffb41
Akash Mishra <leo.akashmishra@gmail.com>
2025-04-01 00:09:33 +0530

    New update

--- routes/casualRoute.js
+++ routes/casualRoute.js
@@ -6,7 +6,18 @@ const axios = require("axios")
 
 router.get("/",async (req,res)=>{
     let ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    res.json({ ip });
+    const geoData = await axios.get(`http://ip-api.com/json/${ip}`);
+    const data = geoData.data
+
+// This is the main route for the API
+    res.status(200).json({
+    msg: "Hello World!",
+    data:data ,
+    name:"Casual API",
+    userRequestIp:req.ip,
+    baseUri:"https://skysync.in"
+    })
+
 })
 
 
